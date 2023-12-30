2729f095b516550b821fdbbac1bb0ef535863568
maouia <maouiabend@gmail.com>
2023-12-30 04:31:11 +0100

    fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,8 +2,11 @@
 """
 initialize the views package
 """
-from api.v1.views.index import *
+
 from flask import Blueprint
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+
+from api.v1.views.index import *
